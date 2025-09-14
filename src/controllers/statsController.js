08c5e3e0263dcc7d7266b4a15000ad4d2414f587
cristhianfelipe89 <cristhianfelipe89@gmail.com>
2025-09-14 14:13:17 -0500

    actualizacion a stats controller

--- src/controllers/statsController.js
+++ src/controllers/statsController.js
@@ -1,14 +1,18 @@
 const Game = require("../models/Game");
 const AnswerLog = require("../models/AnswerLog");
+const User = require("../models/User"); // importa el modelo de usuario
 
 async function overview(_, res) {
-    const [played, winners, categories] = await Promise.all([
+    const [played, winnersRaw, categories] = await Promise.all([
         Game.countDocuments({ status: "finished" }),
+
         Game.aggregate([
             { $match: { winner: { $ne: null } } },
             { $group: { _id: "$winner", wins: { $sum: 1 } } },
-            { $sort: { wins: -1 } }, { $limit: 20 }
+            { $sort: { wins: -1 } },
+            { $limit: 20 }
         ]),
+
         AnswerLog.aggregate([
             { $match: { correct: true } },
             { $lookup: { from: "questions", localField: "question", foreignField: "_id", as: "q" } },
@@ -17,6 +21,17 @@ async function overview(_, res) {
             { $sort: { hits: -1 } }
         ])
     ]);
+
+    // Mapear ganadores con nombre del usuario
+    const winnerIds = winnersRaw.map(w => w._id);
+    const users = await User.find({ _id: { $in: winnerIds } }, "name username");
+    const usersMap = Object.fromEntries(users.map(u => [u._id.toString(), u.name || u.username]));
+
+    const winners = winnersRaw.map(w => ({
+        user: usersMap[w._id.toString()] || w._id,
+        wins: w.wins
+    }));
+
     res.json({ played, winners, categories });
 }
 
